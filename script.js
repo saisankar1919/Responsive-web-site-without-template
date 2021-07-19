1ac0df03e2b8fe0e69bc9d640cdf6ef00da45e23
Sai sankar <65860989+saisankar1919@users.noreply.github.com>
2021-07-19 09:43:56 +0530

    script change

--- script.js
+++ script.js
@@ -1,52 +1,53 @@
+var firstnamestatus, lastnamestatus,emailstatus,phonestatus,messagestatus;
 function firstnamevalid() {
     var name = document.getElementById("fname").value
     var nameerror = document.getElementById("firstnameerror");
-    var letters = /^[a-zA-Z\s]*$/;
-    if (name == "") {
+    var letters = /^[a-zA-Z-()\s]*$/;
+    if (name == "" || name ==null) {
         nameerror.innerHTML = "Field is required...!!!"
-        return false;
+        firstnamestatus = false;
     }
     else if (name.match(letters)) {
         nameerror.innerHTML = "";
-        return true;
+        firstnamestatus = true;
     }
     else {
         nameerror.innerHTML = "Name is invalid...!!!";
-        return false;
+        firstnamestatus = false;
     }
 }
 function lastnamevalid() {
     var name = document.getElementById("lname").value
     var nameerror = document.getElementById("lastnameerror");
     var letters = /^[a-zA-Z\s]*$/;
-    if (name == "") {
+    if (name == "" || name == null) {
         nameerror.innerHTML = "Field is required...!!!"
-        return false;
+        lastnamestatus = false;
     }
     else if (name.match(letters)) {
         nameerror.innerHTML = "";
-        return true;
+        lastnamestatus = true;
     }
     else {
         nameerror.innerHTML = "Name is invalid...!!!";
-        return false;
+        lastnamestatus = false;
     }
 }
 function emailvalid() {
     var email = document.getElementById("email").value
     var emailerror = document.getElementById("emailerror");
     var letters = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
-    if (email == "") {
+    if (email == "" || email == null) {
         emailerror.innerHTML = "Filed is required"
-        return false;
+        emailstatus = false;
     }
     else if (email.match(letters)) {
         emailerror.innerHTML = "";
-        return true;
+        emailstatus = true;
     }
     else {
         emailerror.innerHTML = "invalid email";
-        return false;
+        emailstatus = false;
     }
 
 }
@@ -54,17 +55,17 @@ function phonevalid() {
     var phone = document.getElementById("phone").value
     var phoneerror = document.getElementById("phoneerror");
     const letters = /^\d{10}$/;
-    if (phone == "") {
+    if (phone == "" || phone == null) {
         phoneerror.innerHTML = "Filed is required...!!!"
-        return false;
+        phonestatus = false;
     }
     else if (phone.match(letters)) {
         phoneerror.innerHTML = "";
-        return true;
+        phone  = true;
     }
     else {
         phoneerror.innerHTML = "Number is invalid...!!!";
-        return false;
+        phonestatus = false;
     }
 
 }
@@ -73,15 +74,41 @@ function messagevalid() {
     var message = document.getElementById("message").value
     var messageerror = document.getElementById("messageerror");
 
-    if (message == "") {
+    if (message == " " || message ==null) {
         messageerror.innerHTML = "Filed is required...!!!"
-        return false;
+        messagestatus = false;
     }
 
 
     else {
         messageerror.innerHTML = "";
-        return true;
+        messagestatus = true;
     }
 
 }
+$("#submit-form").submit((e)=>{
+   
+    e.preventDefault()
+  
+
+    if( msg ==true&& realname==true && email==true && phoneStatus==true){
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycby_ZnPvUREK43swDxC6ucBxp9v1QLirqnrrv18AzgK-R6nCyyI65yya3oQ7AxgHlS3iLA/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            
+        },
+        error:function (err){
+            alert("Error")
+
+        }
+    })
+   }
+   else
+   {
+       alert("Error")
+   }
+})
